cf0b8e1faa96ec84f95202f85d69f052c4b96e12
francesco donnini <donnini.francesco00@gmail.com>
2023-06-20 16:49:33 +0200

    Added gitignore

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,11 +1,13 @@
 import json
+import os
 
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_file('config.json', load=json.load)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
