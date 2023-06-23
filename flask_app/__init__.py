0efe8ce987445d6706cfd59a8c60d5013223c389
Francesco Donnini <donnini.francesco00@gmail.com>
2023-06-23 13:42:44 +0000

    changed for deployment

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,8 +3,11 @@ import os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv 
+import os
 
 app = Flask(__name__)
+load_dotenv()
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
 db = SQLAlchemy(app)
